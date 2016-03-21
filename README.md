4b24f6d78b8af073218d849684cb05f028f6b7ad
aaron@pirate-bay <aaron@pirate-bay>
2016-03-20 21:11:12 -0500

    add homebrew instructions to readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 To run the tests:
 
-1.  Ensure you have Python 2.7.x installed and in your path.
+1.  Ensure you have Python 2.7.x installed and in your path (e.g. on OSX - homebrew install python).
 2.  From the root directory, execute `python setup.py test`
--- page/base.py
+++ page/base.py
@@ -9,6 +9,6 @@ class BasePage(object):
 
     def navigate(self, page):
         """Navigate to the given `page` specified by the link name in the top navbar"""
-        
+
         navbar_item = self.driver.find_element(By.LINK_TEXT, page)
         navbar_item.click()
